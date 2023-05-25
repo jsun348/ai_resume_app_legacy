54da41ca35ef040784b82055dcb6e85bde023b85
jsun348 <65511027+jsun348@users.noreply.github.com>
2023-05-25 01:18:40 -0400

    updated for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'docs', // Set the desired output directory to 'docs'
+    base: './', // Add this line to set the base path
   },
 });
